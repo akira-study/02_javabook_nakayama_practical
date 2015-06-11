d1dd3c40875a0be79e51788e5293607f89c583ef
vagrant <vagrant@localhost.localdomain>
2015-06-11 08:31:50 +0000

    list 1-4

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String s1 = "Java programming";
-        System.out.println("文字列s1の3文字目以降は" + s1.substring(3)); // a programming
-        System.out.println("文字列s1の3〜8文字目は" + s1.substring(3, 8)); // a pro
-    
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            sb.append("Java");
+        }
+        String s = sb.toString();
     }
     
 }
